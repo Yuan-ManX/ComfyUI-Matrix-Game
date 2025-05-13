1a57207bcb6bb00a5c10af77de4675d90b1b9a51
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-13 16:04:59 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,23 @@
-from .nodes import PlayGame
+from .nodes import LoadDiTModel, LoadVAEModel, LoadTextEncoderModel, LoadGameImage, LoadMouseIcon, GameVideoGenerator, MatrixGameOutput
 
 NODE_CLASS_MAPPINGS = {
-    "PlayGame": PlayGame,
+    "LoadDiTModel": LoadDiTModel,
+    "LoadVAEModel": LoadVAEModel,
+    "LoadTextEncoderModel": LoadTextEncoderModel,
+    "LoadGameImage": LoadGameImage,
+    "LoadMouseIcon": LoadMouseIcon,
+    "GameVideoGenerator": GameVideoGenerator,
+    "MatrixGameOutput": MatrixGameOutput,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PlayGame": "Play Game",
+    "LoadDiTModel": "Load DiT Model",
+    "LoadVAEModel": "Load VAE Model",
+    "LoadTextEncoderModel": "Load TextEncoder Model",
+    "LoadGameImage": "Load Game Image",
+    "LoadMouseIcon": "Load Mouse Icon",
+    "GameVideoGenerator": "Game Video Generator",
+    "MatrixGameOutput": "MatrixGame Output",
 } 
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
